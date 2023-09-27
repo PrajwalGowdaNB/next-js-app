adb85e94924018d5573861e4f7294570e0030123
Prajwal Gowda N B <prajwal.b@fireflink.com>
2023-09-27 15:08:17 +0530

    first commit(layout and readMd changes)

---
+++ src/common/layouts/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="flex justify-center dark:{text-white} p-4">
+      <p >AIO</p>
+    </header>
+  )
+}
+
+export default Header
